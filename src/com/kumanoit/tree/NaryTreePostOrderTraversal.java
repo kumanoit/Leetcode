28b01add3e57e233781d6d542353318dceb70467
Amit Kumar <kumanoit@Amits-MacBook-Pro.local>
2019-12-25 03:24:51 +0530

    Trees : Easy : N-ary Tree Postorder Traversal

--- src/com/kumanoit/tree/NaryTreePostOrderTraversal.java
+++ src/com/kumanoit/tree/NaryTreePostOrderTraversal.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * https://leetcode.com/problems/n-ary-tree-preorder-traversal/
+ * https://leetcode.com/problems/n-ary-tree-postorder-traversal/
  * 
- * Algorithm: Run preorder recursively on all children of root.
+ * Algorithm: Run postorder recursively on all children of root.
  * 
- * @author kumanoit 22-Dec-2019 8:45:59 PM
+ * @author kumanoit 25-Dec-2019 3:07:34 AM
  */
-public class NAryTreePreorderTraversal {
+public class NaryTreePostOrderTraversal {
 
 	public static void main(String[] args) {
 		test(new Integer[] { 1, null, 3, 2, 4, null, 5, 6 });
@@ -18,27 +18,27 @@ public class NAryTreePreorderTraversal {
 
 	private static void test(final Integer[] nums) {
 		NaryTreeNode root = TreeUtils.createNaryTreeNode(nums);
-		preorder(root).forEach(item -> System.out.print(item + ", "));
+		postorder(root).forEach(item -> System.out.print(item + ", "));
 	}
 
-	public static List<Integer> preorder(NaryTreeNode root) {
+	public static List<Integer> postorder(NaryTreeNode root) {
 		List<Integer> solution = new ArrayList<Integer>();
 		if (root == null) {
 			return solution;
 		}
-		preorder(root, solution);
+		postorder(root, solution);
 		return solution;
 	}
 
-	private static void preorder(NaryTreeNode root, List<Integer> solution) {
+	private static void postorder(NaryTreeNode root, List<Integer> solution) {
 		if (root == null) {
 			return;
 		}
-		solution.add(root.val);
 		if (root.children != null) {
 			for (NaryTreeNode child : root.children) {
-				preorder(child, solution);
+				postorder(child, solution);
 			}
 		}
+		solution.add(root.val);
 	}
 }
