c6c18e2b76ebedaa4c72c4c3919f1b2804cf6cf9
Amit Kumar <akumar@indeed.com>
2020-08-05 00:37:13 +0530

    Arrays : Recursion : Medium : 39. Combination Sum

--- src/com/kumanoit/dynamicprogramming/CombinationSum.java
+++ src/com/kumanoit/dynamicprogramming/CombinationSum.java
@@ -1,83 +1,103 @@
 package com.kumanoit.dynamicprogramming;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
- * https://leetcode.com/problems/combination-sum-iv/
- * 377. Combination Sum IV
- * Approach:
- * Brute Force:
- * Try all possible combination using below recursion formula
- * fun(target, nums) = summation of all values for each num in nums array(fun(target-num, nums))
- * <p>
- * Complexity
- * 1. Time: O(nums.length^target) exponential
- * 2. Space: O(target) recursion will use space on stack. In worst case it will be height of tree when num=1 each time
- * target will get reduced by 1.
- * <p>
- * Here we can see that there are many overlapping sub problems
- * Ex for nums=[1,2,3] and target as 5, fun(5) = fun(5-1=4) + fun(5-2=3) + fun(5-3=2) = fun(4) + fun(3) + fun(2)
- * fun(4) = fun(3) + fun(2) + fun(1)
- * <p>
- * It can be optimized by saving these intermediate results in some kind of lookup table.
- * This is done in dynamic programming function.
- * <p>
- * Complexity:
- * Time: O(target * nums.length) : O(n^2)
- * Space: O(target) for saving results in lookup table of size target
+ * https://leetcode.com/problems/combination-sum/submissions/
+ * 39. Combination Sum
  *
- * @author akumar on 8/4/20 IST 1:15 AM
+ * Approach:
+ * Solve this recursively. Create two variables:
+ * subSolution: list of all integers whose sum is equal to target
+ * solution: which is list of all subSolution
+ * In recursive call, form the list of numbers which sums to target using DFS approach. When target is met save that
+ * subSolution list in solution. Do this for all combinations.
+ *
+ * Complexity
+ * 1. Time: In worst case all combination of number will be checked. For an array of length n, it would be closer to n^k
+ * 2. Space: stack is used which will take space of O(target) if there is an element 1 in array.
+ *
+ * @author akumar on 8/4/20 IST 1:37 AM
  */
 public class CombinationSum {
 
     public static void main(String[] args) {
-        test(0, new int[]{});
-        test(0, new int[]{2});
-        test(3, new int[]{});
-        test(5, new int[]{1, 2, 5});
-        test(3, new int[]{2});
-        test(10, new int[]{10});
-        test(83, new int[]{186, 419, 83, 408});
-        test(186 + 419, new int[]{186, 419, 83, 408});
-        test(408 + 186 + 83, new int[]{186, 419, 83, 408});
-        test(1096, new int[]{186, 419, 83, 408});
-        test(2436, new int[]{186, 419, 83, 408});
-        test(2528, new int[]{186, 419, 83, 408});
+        test(new int[]{2, 3, 5}, 5);
+        test(new int[]{2, 3, 5}, 7);
+        test(new int[]{2, 3, 5}, 4);
+        test(new int[]{2, 3, 5}, 8);
+        test(new int[]{2, 3, 6, 7}, 0);
+        test(new int[]{2, 3, 6, 7}, 7);
     }
 
-    private static void test(int target, int[] nums) {
-        System.out.println("Recursive approach solution: " + getTotalCombinationToGetSumRecursive(nums, target));
-        System.out.println("Dynamic Programming approach solution: " + getTotalCombinationToGetSum(nums, target));
+    private static void test(int[] candidates, int target) {
+        System.out.println("Input: " + Arrays.asList(candidates));
+        System.out.println("Solution 1: ");
+        List<List<Integer>> combinationSum = getCombinationSum(candidates, target);
+        printList(combinationSum);
+
+        List<List<Integer>> solution = new ArrayList<>();
+        calculate(candidates, target, solution, new ArrayList<>(), 0);
+        System.out.println("\nSolution 2: ");
+        printList(solution);
+    }
+
+    private static void printList(final List<List<Integer>> combinationSum) {
+        if (combinationSum != null || !combinationSum.isEmpty()) {
+            combinationSum.forEach(list -> {
+                list.forEach(item -> {
+                    System.out.print(item + ", ");
+                });
+                System.out.println();
+            });
+        } else {
+            System.out.println(combinationSum);
+        }
         System.out.println();
     }
 
-    private static int getTotalCombinationToGetSumRecursive(int[] nums, int target) {
+    private static void calculate(int[] candidates, int target, List<List<Integer>> solution, List<Integer> subSolution,
+                                  int start) {
         if (target == 0) {
-            return 1;
+            solution.add(new ArrayList<>(subSolution));
+            return;
         }
         if (target < 0) {
-            return 0;
+            return;
         }
-        int totalWays = 0;
-        for (int i = 0; i < nums.length; i++) {
-            totalWays += getTotalCombinationToGetSumRecursive(nums, target - nums[i]);
+        for (int i = start; i < candidates.length; i++) {
+            subSolution.add(candidates[i]);
+            calculate(candidates, target - candidates[i], solution, subSolution, i);
+            subSolution.remove(subSolution.size() - 1);
         }
-        return totalWays;
     }
 
-    private static int getTotalCombinationToGetSum(int[] nums, int target) {
-        int[] memo = new int[target + 1];
-        memo[0] = 1;
-        Arrays.sort(nums);
-        for (int i = 1; i <= target; i++) {
-            for (int num : nums) {
-                if (num <= i) {
-                    memo[i] += memo[i - num];
-                } else {
-                    break;
+    private static List<List<Integer>> getCombinationSum(int[] candidates, int target) {
+        Map<Integer, List<List<Integer>>> memo = new HashMap<>();
+        memo.put(0, new ArrayList<>());
+        memo.get(0).add(new ArrayList<>());
+        Arrays.sort(candidates);
+        for (int candidate : candidates) {
+            for (int i = candidate; i <= target; i++) {
+                int diff = i - candidate;
+                if (memo.containsKey(diff)) {
+                    if (!memo.containsKey(i)) {
+                        memo.put(i, new ArrayList<>());
+                    }
+                    List<List<Integer>> presentList = new ArrayList<>();
+                    for (List<Integer> list : memo.get(diff)) {
+                        List<Integer> newlist = new ArrayList<>(list);
+                        newlist.add(candidate);
+                        presentList.add(newlist);
+                    }
+                    memo.get(i).addAll(presentList);
                 }
             }
         }
-        return memo[target];
+        return memo.get(target);
     }
 }
---
+++ src/com/kumanoit/dynamicprogramming/CombinationSum4.java
@@ -0,0 +1,83 @@
+package com.kumanoit.dynamicprogramming;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/combination-sum-iv/
+ * 377. Combination Sum IV
+ * Approach:
+ * Brute Force:
+ * Try all possible combination using below recursion formula
+ * fun(target, nums) = summation of all values for each num in nums array(fun(target-num, nums))
+ * <p>
+ * Complexity
+ * 1. Time: O(nums.length^target) exponential
+ * 2. Space: O(target) recursion will use space on stack. In worst case it will be height of tree when num=1 each time
+ * target will get reduced by 1.
+ * <p>
+ * Here we can see that there are many overlapping sub problems
+ * Ex for nums=[1,2,3] and target as 5, fun(5) = fun(5-1=4) + fun(5-2=3) + fun(5-3=2) = fun(4) + fun(3) + fun(2)
+ * fun(4) = fun(3) + fun(2) + fun(1)
+ * <p>
+ * It can be optimized by saving these intermediate results in some kind of lookup table.
+ * This is done in dynamic programming function.
+ * <p>
+ * Complexity:
+ * Time: O(target * nums.length) : O(n^2)
+ * Space: O(target) for saving results in lookup table of size target
+ *
+ * @author akumar on 8/4/20 IST 1:15 AM
+ */
+public class CombinationSum4 {
+
+    public static void main(String[] args) {
+        test(0, new int[]{});
+        test(0, new int[]{2});
+        test(3, new int[]{});
+        test(5, new int[]{1, 2, 5});
+        test(3, new int[]{2});
+        test(10, new int[]{10});
+        test(83, new int[]{186, 419, 83, 408});
+        test(186 + 419, new int[]{186, 419, 83, 408});
+        test(408 + 186 + 83, new int[]{186, 419, 83, 408});
+        test(1096, new int[]{186, 419, 83, 408});
+        test(2436, new int[]{186, 419, 83, 408});
+        test(2528, new int[]{186, 419, 83, 408});
+    }
+
+    private static void test(int target, int[] nums) {
+        System.out.println("Recursive approach solution: " + getTotalCombinationToGetSumRecursive(nums, target));
+        System.out.println("Dynamic Programming approach solution: " + getTotalCombinationToGetSum(nums, target));
+        System.out.println();
+    }
+
+    private static int getTotalCombinationToGetSumRecursive(int[] nums, int target) {
+        if (target == 0) {
+            return 1;
+        }
+        if (target < 0) {
+            return 0;
+        }
+        int totalWays = 0;
+        for (int i = 0; i < nums.length; i++) {
+            totalWays += getTotalCombinationToGetSumRecursive(nums, target - nums[i]);
+        }
+        return totalWays;
+    }
+
+    private static int getTotalCombinationToGetSum(int[] nums, int target) {
+        int[] memo = new int[target + 1];
+        memo[0] = 1;
+        Arrays.sort(nums);
+        for (int i = 1; i <= target; i++) {
+            for (int num : nums) {
+                if (num <= i) {
+                    memo[i] += memo[i - num];
+                } else {
+                    break;
+                }
+            }
+        }
+        return memo[target];
+    }
+}
